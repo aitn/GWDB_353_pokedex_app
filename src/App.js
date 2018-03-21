baaa88923a2d28778828400147af53cb98597dc1
Joel Norris <joel.r.norris@gmail.com>
2018-03-20 19:24:56 -0500

    added tabs, classed out tab components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import TabsInterface from './TabsInterface';
+import Background from './Background';
+
+const API = 'http://localhost:3000/api/v1/';
+const DEFAULT_QUERY = 'pokemons';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="Pokedex-App">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello World</h1>
-        </header>
-        <p className="App-intro">
-            Welcome to my FAN~foxtrot~tabulous pokedex APP
-        </p>
-      </div>
-    );
-  }
-}
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: []
+        };
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+
+      return (
+        <div className="App">
+          <header className="Pokedex-App">
+            <h1 className="App-title">Hello World</h1>
+              <p className="App-intro">
+                  Welcome to my FAN~foxtrotting~tabulous pokedex APP
+              </p>
+          </header>
+
+            <TabsInterface />
+
+            {/*<Background />*/}
+
+        </div>
+      );
+    }
+}
 
 
 export default App;
